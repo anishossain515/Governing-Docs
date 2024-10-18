c24f1a71a57cbb6bcead5c885b53b815ca89dd1a
anishossain515 <amirul447700@gmail.com>
2024-10-19 02:45:56 +0600

    Partners Section Done

--- GoverningDocs/src/App.jsx
+++ GoverningDocs/src/App.jsx
@@ -1,10 +1,10 @@
 import Hero from "./components/contains/Hero"
 import MobileMenu from "./components/contains/Navbar/MobileMenu"
 import Navbar from "./components/contains/Navbar/Navbar"
+import { Partners } from "./components/contains/Partners"
 import ProblemSolution from "./components/contains/ProblemSolution/ProblemSolution"
 import ProblemSolutionHeader from "./components/contains/ProblemSolution/ProblemsolutionHeader"
 import { Reviews } from "./components/contains/Reviews/Reviews"
-
 
 
 function App() {
@@ -14,6 +14,7 @@ function App() {
        <Hero/>  
        <ProblemSolution/>
        <Reviews/>
+       <Partners/>
     </main>
   )
 }
---
+++ GoverningDocs/src/components/contains/Partners.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Container from "../common/Container";
+import Heading from "../common/Heading";
+import PartnersList from "../../lib/utilitys/Data.json";
+
+export const Partners = () => {
+  return (
+    <section className="mt-[150px] bg-[#e6f9fd]">
+      <Container className="py-[50px] px-[45px]">
+        <Heading gradient="Partners" className="text-center text-4xl">
+          Our
+        </Heading>
+
+        <div className="flex flex-wrap items-center justify-center gap-[30px] pt-10">
+          {PartnersList.Partners.map(({ image, name }) => (
+            <div className="bg-white w-[274px] h-[101px] rounded-xl border border-[#d6d6d6] flex items-center justify-center ">
+              <img src={image} alt={name} />
+            </div>
+          ))}
+        </div>
+      </Container>
+    </section>
+  );
+};
